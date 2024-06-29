a877c0f5aa31d1c572199b615473163933a96cc1
Фёдор <fedor.pavlov.02@mail.ru>
2024-06-30 00:58:08 +0700

    Add first iteration of webview
    Небольшой рефакторинг и добавлена первая итерация отдельного окна (webview) для расширения, пока только с кнопкой запуска проверки.

--- src/extension.ts
+++ src/extension.ts
@@ -1,84 +1,115 @@
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
 import * as os from 'os';
+import * as path from 'path';
+import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) 
 {
 	console.log('Congratulations, your extension "DependencyCheck" is now active!');
 
-	const execShell = (cmd: string, options: cp.ExecOptions = {}) =>
+	let currentPanel: vscode.WebviewPanel | undefined = undefined;
+
+	const execShell = (cmd: string, options: cp.ExecOptions = {}, onProgress?: (progress: number) => void) =>
 		new Promise<string>((resolve, reject) => {
-			cp.exec(cmd, options, (err, out) => {
+			const process = cp.exec(cmd, options, (err, out) => {
 				if (err) {
 						return reject(err);
 				}
 				return resolve(out);
 			});
+
+			if (onProgress) {
+				process.stdout?.on('data', (data) => {
+					const progressMatch = data.match(/Progress:\s+(\d+)%/);
+					if (progressMatch) {
+							onProgress(parseInt(progressMatch[1], 10));
+					}
+				});
+			}
 		});
 
-	
 	const runDependencyCheckCommand = vscode.commands.registerCommand('dependency-check.runDependencyCheck', 
 	async () => 
 	{
-		const config = vscode.workspace.getConfiguration('dependency-check');
-		const pathToDC = config.get<string>('pathToDC', '');
-		if (pathToDC.length === 0) {
-			vscode.window.showInformationMessage("Set options first, path to DC is empty");
-			return;
-		}
-
-		let projectPath = '';
-		if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
-			projectPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-			vscode.window.showInformationMessage(`Current project: ${projectPath}`);
+		if (currentPanel) {
+			currentPanel.reveal(vscode.ViewColumn.One);
 		} else {
-			vscode.window.showErrorMessage('No project folder is currently open');
-			return;
+			currentPanel = vscode.window.createWebviewPanel(
+				'dependency-check',
+				'Dependency Check',
+				vscode.ViewColumn.One,
+				{
+					enableScripts: true
+				}
+			);
 		}
+		const htmlPath = path.join(context.extensionPath, 'webview.html');
+		const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+		currentPanel.webview.html = htmlContent;
 
-		let osDCCommand = '';
-		if (os.platform() === 'win32') {
-			osDCCommand = `dependency-check.bat --project "Dependency Check" --scan "${projectPath}" --out "${projectPath}"`;
-		} else {
-			osDCCommand = `./dependency-check.sh --project "Dependency Check" --scan "${projectPath}" --out "${projectPath}"`;
-		}
+		currentPanel.webview.onDidReceiveMessage(
+			async message => {
+				switch (message.command) {
+					case 'runDependencyCheck':
+						const dcData = startDC();
+						if (!dcData) {return;}
+						const { pathToDC, osDCCommand } = dcData;
 
-		try {
-			vscode.window.showInformationMessage('Started Dependency Check');
-			await execShell(osDCCommand, { cwd: pathToDC });
-			vscode.window.showInformationMessage('Dependency Check finished');
-		} catch (error) {
-			vscode.window.showErrorMessage(`Error: ${(error as Error).message}`);
-		}
+						try {
+							vscode.window.showInformationMessage('Started Dependency Check');
+							await execShell(osDCCommand, { cwd: pathToDC }, (progress) => {
+								currentPanel?.webview.postMessage({ command: 'updateProgress', progress });
+							});
+							vscode.window.showInformationMessage('Dependency Check finished');
+						} catch (error) {
+							vscode.window.showErrorMessage(`Error: ${(error as Error).message}`);
+						}
+						break;
+						
+				}
+			},
+			undefined,
+			context.subscriptions
+		);
+
+		currentPanel.onDidDispose(
+			() => {
+				currentPanel = undefined;
+			},
+			null,
+			context.subscriptions
+		);
 	});
 
-	const setOptionsCommand = vscode.commands.registerCommand('dependency-check.setOptions', 
-	async () => 
-	{
-		const config = vscode.workspace.getConfiguration('dependency-check');
-		const userInputDC = await vscode.window.showInputBox({
-			prompt: 'Enter path to dependency-check/bin folder',
-			placeHolder: 'Type here'
-		});
-
-		if (userInputDC) {
-			vscode.window.showInformationMessage(`You entered: ${userInputDC}`);
-			await config.update('pathToDC', userInputDC, vscode.ConfigurationTarget.Global);
-		}
-
-		const userInputMaven = await vscode.window.showInputBox({
-			prompt: 'Enter path to Maven executable',
-			placeHolder: 'Type here'
-		});
-
-		if (userInputMaven) {
-			vscode.window.showInformationMessage(`You entered: ${userInputMaven}`);
-			await config.update('pathToMaven', userInputMaven, vscode.ConfigurationTarget.Global);
-		}
-
-	});
-
-	context.subscriptions.push(runDependencyCheckCommand, setOptionsCommand);
+	context.subscriptions.push(runDependencyCheckCommand);
 }
 
 export function deactivate() {}
+
+function startDC(): {pathToDC: string, osDCCommand: string} | undefined {
+	const config = vscode.workspace.getConfiguration('dependency-check');
+	const pathToDC = config.get<string>('pathToDC', '');
+	if (pathToDC.length === 0) {
+		vscode.window.showInformationMessage("Set options first, path to DC is empty");
+		return;
+	}
+
+	let projectPath = '';
+	if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
+		projectPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		vscode.window.showInformationMessage(`Current project: ${projectPath}`);
+	} else {
+		vscode.window.showErrorMessage('No project folder is currently open');
+		return;
+	}
+
+	let osDCCommand = '';
+	if (os.platform() === 'win32') {
+		osDCCommand = `dependency-check.bat --project "Dependency Check" --scan "${projectPath}" --out "${projectPath}" --noupdate --prettyPrint --format "JSON"`;// add "--noupdate" in case of no response
+	} else {
+		osDCCommand = `./dependency-check.sh --project "Dependency Check" --scan "${projectPath}" --out "${projectPath}" --noupdate --prettyPrint --format "JSON"`;
+	}
+
+	return {pathToDC, osDCCommand};
+}
