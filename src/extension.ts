18188473650be68931dbbe40350a34f4d7a15721
Фёдор <fedor.pavlov.02@mail.ru>
2024-06-30 15:44:12 +0700

    Progressbar done

--- src/extension.ts
+++ src/extension.ts
@@ -10,95 +10,89 @@ export function activate(context: vscode.ExtensionContext)
 
 	let currentPanel: vscode.WebviewPanel | undefined = undefined;
 
-	const execShell = (cmd: string, options: cp.ExecOptions = {}, onProgress?: (progress: number) => void) =>
+	const execShell = (cmd: string, options: cp.ExecOptions = {}) =>
 		new Promise<string>((resolve, reject) => {
-			const process = cp.exec(cmd, options, (err, out) => {
-				if (err) {
-						return reject(err);
-				}
+			cp.exec(cmd, options, (err, out) => {
+				if (err) {return reject(err);}
 				return resolve(out);
 			});
-
-			if (onProgress) {
-				process.stdout?.on('data', (data) => {
-					const progressMatch = data.match(/Progress:\s+(\d+)%/);
-					if (progressMatch) {
-							onProgress(parseInt(progressMatch[1], 10));
-					}
-				});
-			}
 		});
 
-	const runDependencyCheckCommand = vscode.commands.registerCommand('dependency-check.runDependencyCheck', 
+	const showExtensionWindowCommand = vscode.commands.registerCommand('dependency-check.showExtensionWindow', 
 	async () => 
 	{
+		const columnToShowIn = vscode.window.activeTextEditor
+		? vscode.window.activeTextEditor.viewColumn
+		: undefined;
+
 		if (currentPanel) {
-			currentPanel.reveal(vscode.ViewColumn.One);
+			currentPanel.reveal(columnToShowIn);
 		} else {
 			currentPanel = vscode.window.createWebviewPanel(
 				'dependency-check',
 				'Dependency Check',
-				vscode.ViewColumn.One,
+				columnToShowIn || vscode.ViewColumn.One,
 				{
-					enableScripts: true
+					enableScripts: true,
+					retainContextWhenHidden: true
 				}
 			);
+
+			const htmlPath = path.join(context.extensionPath, 'webview.html');
+			const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+			currentPanel.webview.html = htmlContent;
+
+			currentPanel.webview.onDidReceiveMessage(
+				async message => {
+					switch (message.command) {
+						case 'runDependencyCheck':
+							const dcData = setConfigurationForDC();
+							if (!dcData) {return;}
+							const { pathToDC, osDCCommand } = dcData;
+
+							currentPanel?.webview.postMessage({ command: 'startFakeProgress' });
+							try {
+								vscode.window.showInformationMessage('Started Dependency Check');
+								await execShell(osDCCommand, { cwd: pathToDC });
+								currentPanel?.webview.postMessage({ command: 'finishFakeProgress' });
+							} catch (error) {
+								vscode.window.showErrorMessage(`Error: ${(error as Error).message}`);
+								currentPanel?.webview.postMessage({ command: 'errorInFakeProgress' });
+							}
+							break;
+					}
+				},
+				undefined,
+				context.subscriptions
+			);
+
+			currentPanel.onDidDispose(
+				() => {
+					currentPanel = undefined;
+				},
+				null,
+				context.subscriptions
+			);
 		}
-		const htmlPath = path.join(context.extensionPath, 'webview.html');
-		const htmlContent = fs.readFileSync(htmlPath, 'utf8');
-		currentPanel.webview.html = htmlContent;
-
-		currentPanel.webview.onDidReceiveMessage(
-			async message => {
-				switch (message.command) {
-					case 'runDependencyCheck':
-						const dcData = startDC();
-						if (!dcData) {return;}
-						const { pathToDC, osDCCommand } = dcData;
-
-						try {
-							vscode.window.showInformationMessage('Started Dependency Check');
-							await execShell(osDCCommand, { cwd: pathToDC }, (progress) => {
-								currentPanel?.webview.postMessage({ command: 'updateProgress', progress });
-							});
-							vscode.window.showInformationMessage('Dependency Check finished');
-						} catch (error) {
-							vscode.window.showErrorMessage(`Error: ${(error as Error).message}`);
-						}
-						break;
-						
-				}
-			},
-			undefined,
-			context.subscriptions
-		);
-
-		currentPanel.onDidDispose(
-			() => {
-				currentPanel = undefined;
-			},
-			null,
-			context.subscriptions
-		);
 	});
 
-	context.subscriptions.push(runDependencyCheckCommand);
+	context.subscriptions.push(showExtensionWindowCommand);
 }
 
 export function deactivate() {}
 
-function startDC(): {pathToDC: string, osDCCommand: string} | undefined {
+function setConfigurationForDC(): {pathToDC: string, osDCCommand: string} | undefined {
 	const config = vscode.workspace.getConfiguration('dependency-check');
 	const pathToDC = config.get<string>('pathToDC', '');
 	if (pathToDC.length === 0) {
-		vscode.window.showInformationMessage("Set options first, path to DC is empty");
+		vscode.window.showErrorMessage("Set extension options first, path to DC 'bin' folder is empty");
 		return;
 	}
 
 	let projectPath = '';
 	if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
 		projectPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-		vscode.window.showInformationMessage(`Current project: ${projectPath}`);
+		//vscode.window.showInformationMessage(`Current project: ${projectPath}`);
 	} else {
 		vscode.window.showErrorMessage('No project folder is currently open');
 		return;
